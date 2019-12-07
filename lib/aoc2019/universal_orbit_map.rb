9df474041efb7357aee252738670c60f36119504
Robert Haines <rhaines@manchester.ac.uk>
2019-12-07 11:06:32 +0000

    Solution for day 6, part 2.

--- lib/aoc2019/universal_orbit_map.rb
+++ lib/aoc2019/universal_orbit_map.rb
@@ -14,6 +14,7 @@ module AOC2019
       map = build_orbit_map(read_input_file)
 
       puts "Part 1: #{orbit_depths(map).sum}"
+      puts "Part 2: #{count_orbital_moves(map)}"
     end
 
     def orbit_depths(map, root = 'COM', val = 0, acc = [])
@@ -27,6 +28,57 @@ module AOC2019
       end
 
       acc
+    end
+
+    def path_to_node(map, node, acc, root = 'COM')
+      return false if root.nil?
+
+      acc << root
+      return true if root == node
+
+      moons = map[root]
+      if moons.nil?
+        # If there are no moons the root of this map is not in the path.
+        # Remove it.
+        acc.pop
+        return false
+      end
+
+      if path_to_node(map, node, acc, moons[0]) ||
+         (!moons[1].nil? && path_to_node(map, node, acc, moons[1]))
+        return true
+      end
+
+      # If we get here then the root of this map is not in the path. Remove it.
+      acc.pop
+      false
+    end
+
+    def lowest_common_ancestor(map, r = 'COM', n1 = 'YOU', n2 = 'SAN')
+      p1 = []
+      p2 = []
+
+      return if !path_to_node(map, n1, p1, r) || !path_to_node(map, n2, p2, r)
+
+      i = 0
+      loop do
+        break if p1[i] != p2[i]
+
+        i += 1
+      end
+
+      p1[i - 1]
+    end
+
+    def count_orbital_moves(map)
+      root = lowest_common_ancestor(map)
+      you_path = []
+      san_path = []
+
+      path_to_node(map, 'YOU', you_path, root)
+      path_to_node(map, 'SAN', san_path, root)
+
+      you_path.length + san_path.length - 4
     end
 
     def build_orbit_map(input)
--- test/universal_orbit_map_test.rb
+++ test/universal_orbit_map_test.rb
@@ -22,6 +22,18 @@ class AOC2019::UniversalOrbitMapTest < Minitest::Test
     'K' => ['L']
   }.freeze
 
+  SAN_MAP = {
+    'COM' => ['B'],
+    'B' => ['C', 'G'],
+    'C' => ['D'],
+    'D' => ['E', 'I'],
+    'E' => ['F', 'J'],
+    'G' => ['H'],
+    'J' => ['K'],
+    'K' => ['L', 'YOU'],
+    'I' => ['SAN']
+  }.freeze
+
   def setup
     @uom = ::AOC2019::UniversalOrbitMap.new
   end
@@ -32,5 +44,19 @@ class AOC2019::UniversalOrbitMapTest < Minitest::Test
 
   def test_orbit_depths
     assert_equal(42, @uom.orbit_depths(MAP).sum)
+  end
+
+  def test_path_to_node
+    path = []
+    @uom.path_to_node(SAN_MAP, 'YOU', path)
+    assert_equal(['COM', 'B', 'C', 'D', 'E', 'J', 'K', 'YOU'], path)
+  end
+
+  def test_lca
+    assert_equal('D', @uom.lowest_common_ancestor(SAN_MAP))
+  end
+
+  def test_count_orbital_moves
+    assert_equal(4, @uom.count_orbital_moves(SAN_MAP))
   end
 end
