0828936a32e7d3ce8839cf88b3f63497201fb571
Robert Haines <rhaines@manchester.ac.uk>
2019-12-19 22:04:54 +0000

    Solution for day 19, part 2.

--- lib/aoc2019/tractor_beam.rb
+++ lib/aoc2019/tractor_beam.rb
@@ -13,7 +13,12 @@ module AOC2019
   class TractorBeam < Day
     def run
       computer = Common::IntcodeComputer.new(read_input_file.chomp)
+
       puts "Part 1: #{scan(computer, 50, 50).flatten.count(1)}"
+
+      computer.reset!
+      x, y = rectangle(computer, 100)
+      puts "Part 2: #{x * 10_000 + y}"
     end
 
     def scan(computer, x_dim, y_dim)
@@ -28,6 +33,27 @@ module AOC2019
       end
 
       output
+    end
+
+    def rectangle(computer, size)
+      x = 1_500
+      y = 1_000
+      size -= 1
+
+      loop do
+        loop do
+          computer.reset!
+          if computer.run(x, y).first.zero?
+            y += 1
+            next
+          end
+
+          computer.reset!
+          return [x - size, y] if computer.run(x - size, y + size).first == 1
+
+          x += 1
+        end
+      end
     end
   end
 end
