5689c01af99e123e05d28b2c9f6d069c890ab14a
Robert Haines <rhaines@manchester.ac.uk>
2019-12-23 19:54:28 +0000

    Solution for day 21, part 2.

--- lib/aoc2019/springdroid_adventure.rb
+++ lib/aoc2019/springdroid_adventure.rb
@@ -11,7 +11,7 @@ require_relative 'common/intcode_computer'
 
 module AOC2019
   class SpringdroidAdventure < Day
-    SCRIPT = ([
+    WALK = ([
       'NOT C T',
       'NOT A J',
       'OR T J',
@@ -19,10 +19,29 @@ module AOC2019
       'WALK'
     ].join("\n") + "\n").chars.map(&:ord)
 
+    RUN = ([
+      'OR H J',
+      'NOT C T',
+      'AND T J',
+      'NOT T T',
+      'OR F T',
+      'NOT T T',
+      'OR T J',
+      'NOT B T',
+      'OR T J',
+      'NOT A T',
+      'OR T J',
+      'AND D J',
+      'RUN'
+    ].join("\n") + "\n").chars.map(&:ord)
+
     def run
       computer = Common::IntcodeComputer.new(read_input_file)
 
-      puts "Part 1: #{computer.run(SCRIPT)[-1]}"
+      puts "Part 1: #{computer.run(WALK)[-1]}"
+
+      computer.reset!
+      puts "Part 2: #{computer.run(RUN)[-1]}"
     end
   end
 end
