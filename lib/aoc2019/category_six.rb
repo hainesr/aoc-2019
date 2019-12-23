b416b597c98c9d269eaad98016ec9eab775cf21f
Robert Haines <rhaines@manchester.ac.uk>
2019-12-23 10:04:05 +0000

    Solution for day 23, part 2.

--- lib/aoc2019/category_six.rb
+++ lib/aoc2019/category_six.rb
@@ -13,7 +13,9 @@ module AOC2019
   class CategorySix < Day
     def run
       code = read_input_file
+
       puts "Part 1: #{run_network(code)}"
+      puts "Part 2: #{run_network(code, 2)}"
     end
 
     def run_network(nic, part = 1)
@@ -26,24 +28,41 @@ module AOC2019
         input_queues[i] << i
       end
 
+      nat_input = []
+      nat_y_save = -1
+
       loop do
+        idle = true
+
         (0...num).each do |i|
           if input_queues[i].empty?
             input = -1
           else
             input = input_queues[i]
             input_queues[i] = []
+            idle = false
           end
 
           output = computers[i].run(input)
           next if output.empty?
 
           output.each_slice(3) do |address, x, y|
-            return y if address == 255 && part == 1
+            if address == 255
+              return y if part == 1
 
-            input_queues[address] << x << y
+              nat_input = [x, y]
+            else
+              input_queues[address] << x << y
+            end
           end
         end
+
+        next unless idle
+
+        input_queues[0] = nat_input
+        return nat_y_save if nat_input[1] == nat_y_save
+
+        nat_y_save = nat_input[1]
       end
     end
   end
